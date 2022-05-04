afd8d18a59812246022a92282fe9c51d4e17cf15
Jan Boerman <janboerman95@gmail.com>
2022-05-04 13:31:41 +0200

    Explain what code to expect in FileIO

--- src/main/scala/nl/alpedhorst/teamscheduling/FileIO.scala
+++ src/main/scala/nl/alpedhorst/teamscheduling/FileIO.scala
@@ -1,3 +1,4 @@
 package nl.alpedhorst.teamscheduling
 
-//TODO read csv file
+//TODO read csv file (contains team names and blacklisted timeslots)
+//TODO write schedule file (not sure yet which format, but probably also csv)
